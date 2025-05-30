7f283c05b538d18d6f910192778f3c42cb42fe7a
isaiahrotich51@gmail.com <123354788+angey200@users.noreply.github.com>
2025-05-30 14:39:32 +0300

    version2

--- index.js
+++ index.js
@@ -32,3 +32,35 @@ function animate() {
   }
 }
 animate();
+
+window.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    const overlay = document.getElementById("loading-overlay");
+    overlay.style.opacity = "0";
+    setTimeout(() => (overlay.style.display = "none"), 600);
+  }, 5000);
+});
+
+const loadingText = [
+  "Empowering Ideas, Delivering Digital Excellence.",
+  "Please wait while we prepare your experience...",
+];
+const loadingElement = document.getElementById("loading-animated-text");
+let loadingLine = 0,
+  loadingChar = 0;
+
+function typeLoadingText() {
+  if (loadingLine < loadingText.length) {
+    if (loadingChar < loadingText[loadingLine].length) {
+      loadingElement.innerHTML += loadingText[loadingLine].charAt(loadingChar);
+      loadingChar++;
+      setTimeout(typeLoadingText, 40);
+    } else {
+      if (loadingLine === 0) loadingElement.innerHTML += "<br>";
+      loadingLine++;
+      loadingChar = 0;
+      setTimeout(typeLoadingText, 500);
+    }
+  }
+}
+typeLoadingText();
